d4e9d61bab325256dd37eadf0d75dbefd0e6e210
daquioag <aldaquioag@gmail.com>
2023-10-29 00:07:37 -0700

    implement the deal function

--- card/lib/card_web/controllers/card_controller.ex
+++ card/lib/card_web/controllers/card_controller.ex
@@ -2,9 +2,11 @@ defmodule CardWeb.CardController do
   use CardWeb, :controller
 
   def value(conn, _param) do
-    IO.inspect(conn)
-    value = Card.Worker.value()
-    send_resp(conn, 200, value)
+    cards = Card.Worker.value()
+    formatted_cards = convert_to_list(cards)
+    IO.inspect(formatted_cards)
+
+    json(conn, formatted_cards)
   end
 
   def count(conn, _params) do
@@ -19,7 +21,32 @@ defmodule CardWeb.CardController do
 
   def new(conn, _params) do
     Card.Worker.new()
-    send_resp(conn, 200, "Spawned new deck")
+    send_resp(conn, 200, "Spawned nessw deck")
   end
+
+  def deal(conn, params) do
+    qty = get_quantity(params)
+    {status, cards} = deal_cards(qty)
+    json(conn, %{"status" => status, "dead cards" => cards})
+  end
+
+
+defp deal_cards(qty) do
+  case Card.Worker.deal(qty) do
+    {:ok, cards} -> {"ok", convert_to_list(cards)}
+    {:error, reason} -> {"error", reason}
+  end
+end
+
+defp get_quantity(%{"qty" => qty}) when is_binary(qty), do: String.to_integer(qty)
+defp get_quantity(%{"qty" => qty}) when is_integer(qty), do: qty
+defp get_quantity(_), do: 1
+
+defp convert_to_list(cards) do
+  formatted_cards = Enum.map(cards, fn {value, suit} ->
+    "#{value} #{suit}"
+  end)
+  formatted_cards
+end
 
 end
